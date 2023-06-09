f2832cb6d54abbe81d9374d6666c9ff842dbf740
O2C14 <70356077+O2C14@users.noreply.github.com>
2023-06-09 10:53:16 +0800

    Update hook.js

--- hook.js
+++ hook.js
@@ -1,12 +1,54 @@
-var BaseAddr;
+function alog(arg) {
+    console.log(arg);
+}
+var BaseAddr = null;
 function main() {
     rpc.exports = {
         start: function (processname) {
-            console.log('脚本加载成功');
-            BaseAddr = Module.getBaseAddress(processname);
-            console.log("基址:", BaseAddr); 
-            hooktest();
+            alog('脚本加载成功');
+            while (BaseAddr === null) {
+                console.log("尝试查找" + processname + "的基址");
+                BaseAddr = Module.findBaseAddress(processname);
+            }
+            alog("基址: " + BaseAddr);
+            //hook_function();
+            //hooktest();
+            setTimeout(sendwidget, 3000);
+        },
+        message:function(messagedic){
+            alog(messagedic);
         }
     };
+}
+var QWidget = {
+    "modmenu": [
+        {
+            "type": "Label",
+            "text": "JS菜单",
+            "x": 50,
+            "y": 100,
+            "width": 100,
+            "height": 30
+        },
+        {
+            "type": "Buttons",
+            "text": "js按钮",
+            "x": 50,
+            "y": 100,
+            "width": 100,
+            "height": 30
+        }
+    ]
+
+}
+function sendwidget() {
+    send({
+        'widget': JSON.stringify(QWidget)
+    });
+}
+function sendmessage(message) {
+    send({
+        'message': message
+    });
 }
 main();
